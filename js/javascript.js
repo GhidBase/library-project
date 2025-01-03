06ed763d39f65ed2817863ddc3778469a44153aa
GhidBase <grvdylans@gmail.com>
2025-01-03 14:10:11 -0700

    Add basic styling to the page

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,12 @@
-console.log("Sup");
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+
+}
