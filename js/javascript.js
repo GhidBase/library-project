54a0a1427f8d6e5ca2b19231b0972d6d66d28625
GhidBase <grvdylans@gmail.com>
2025-01-03 21:02:02 -0700

    addBookToLibrary manipulates the DOM now

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const libraryElement = document.querySelector("#library");
+
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
@@ -9,9 +11,20 @@ function Book(title, author, pages, readStatus) {
 
 function addBookToLibrary(title, author, pages, readStatus) {
     myLibrary.push(new Book(title, author, pages, readStatus));
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("book");
+    libraryElement.appendChild(newDiv);
+}
+
+function initializeLibrary() {
+    for (const book of myLibrary) {
+        console.log(book.title)
+    }
 }
 
 addBookToLibrary("The Way of the Wolf", "Jordan Belfort", 256, true);
 addBookToLibrary("Magic Tree House: Dinosaurs Before Dark", "Mary Pope Osborn", 80, true);
 
-console.log(myLibrary);
+initializeLibrary();
+
+console.log(libraryElement);
